23bbb6316b28a89a9a593801bbf2d99655cb8e86
fokrule <fokrule.hasan@gmail.com>
2022-08-17 18:26:33 -0400

    Moving slice

--- includes/script.js
+++ includes/script.js
@@ -10,10 +10,12 @@ function dropDrag (e) {
     e.preventDefault();
     var data = e.dataTransfer.getData("text");
     var movedSlice = Number((e.target.className).slice(-1));
+    console.log(movedSlice)
     var comingFrom = Number((document.getElementById(data).parentElement.className.slice(-1)));
     var sliceDifferent = movedSlice - comingFrom;
-    var allowedSlice = [-1, 1, 3];
+    var allowedSlice = [-1, 1, 3,-3];
     var isAllowed = allowedSlice.includes(sliceDifferent);
+    console.log(isAllowed)
     if ( (isAllowed) && (document.getElementById(e.target.id).innerHTML.replace(/\s+/g, "").length == 0) ) {
         e.target.appendChild(document.getElementById(data));
     }
